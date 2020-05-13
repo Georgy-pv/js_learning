15571a732278a982e528b9c61cdb9c7069c5c741
Georgy <goshan31458@gmail.com>
2020-05-13 16:21:04 +0900

    Рисунок робота с помощью методоа fillRect() в файле canvas.js

--- 13 Графика. Canvas/canvas.js
+++ 13 Графика. Canvas/canvas.js
@@ -4,9 +4,15 @@ document.addEventListener('DOMContentLoaded', function(){
 
 	let ctx = canvas.getContext("2d");
 	
-	for(var i = 0; i < 8; i++){
-		ctx.fillRect(i*10, i*10, 10, 10);
-	}
-    
+	ctx.fillRect(50,0,50,50);    
+	ctx.fillRect(70,50,10,20); 
+
+	ctx.fillRect(0,70,40,10);    
+	ctx.fillRect(110,70,40,10);    
+	
+	ctx.fillRect(40,70,70,70);
+
+	ctx.fillRect(40,140,10,50);    
+	ctx.fillRect(100,140,10,50);    
 
 });
